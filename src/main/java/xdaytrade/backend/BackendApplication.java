bc410415bbec76972f1b1c90f26ace9f59e81301
liyan61 <375257970@qq.com>
2021-01-08 16:00:08 +0800

    run

--- src/main/java/xdaytrade/backend/BackendApplication.java
+++ src/main/java/xdaytrade/backend/BackendApplication.java
@@ -1,11 +1,12 @@
 package xdaytrade.backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+@MapperScan("xdaytrade.backend.mapper")
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
