669e09684bd5e191b7410f01d2d80683cf1c0080
liyan61 <375257970@qq.com>
2021-01-08 17:01:45 +0800

    user

--- src/main/java/xdaytrade/backend/model/User.java
+++ src/main/java/xdaytrade/backend/model/User.java
@@ -1,5 +1,8 @@
 package xdaytrade.backend.model;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -8,5 +11,38 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 @TableName("user")
 public class User {
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 用户名
+     */
+    @TableField("username")
+    private String username;
+
+    /**
+     * 密码
+     */
+    @TableField("password")
+    private String password;
+
+    /**
+     * 电话号码
+     */
+    @TableField("phone")
+    private String phone;
+
+    /**
+     * 电子邮箱
+     */
+    @TableField("email")
+    private String email;
+
+
+
+
+
+
 
 }
